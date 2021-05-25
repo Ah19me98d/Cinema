5af43ec2ab4e032548ace5250822047d51a39ca7
Ah19me98d <84751391+Ah19me98d@users.noreply.github.com>
2021-05-25 19:46:18 +0100

    Add files via upload

---
+++ cinema/src/main/java/ma/emsi/cinema/dao/VilleRepository.java
@@ -0,0 +1,9 @@
+package ma.emsi.cinema.dao;
+
+import ma.emsi.cinema.entities.Ville;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
+public interface VilleRepository extends JpaRepository<Ville, Long>{
+
+}
